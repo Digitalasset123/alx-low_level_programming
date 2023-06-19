03e649ac66805f9d4a360ec8f57c1b6ec7075750
Godswill Chijioke <luckyjoshua321@gmail.com>
2023-06-18 18:09:05 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Description: Prints a specific string using the puts function
+ *
+ *  Return: always 0 (success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
