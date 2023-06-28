caf1e36c8c388ae51fef94e2e3dfbde382d9d630
Godswill Chijioke <luckyjoshua321@gmail.com>
2023-06-27 18:51:45 -0700

    A program that generates random valid passwords for the program

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Tada! Congrats.\n");
+	return (0);
+}
