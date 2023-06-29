9e54a2a3e45ab1917169866e1aefa3f4caf9c4eb
Godswill Chijioke <luckyjoshua321@gmail.com>
2023-06-29 06:33:27 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: 0 if the strings are equal, a negative value if s1 is less than s2,
+ *         or a positive value if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	/* Iterate through the strings until a difference is found */
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+
+	/* The strings are equal */
+	return (0);
+}
