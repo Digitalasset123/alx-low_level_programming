215fe45a88f483cf31af21f50b301ff6fb859985
root <Godswill>
2023-12-07 19:39:18 -0800

    a function that adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,14 +8,14 @@
  *
  * Return: address of the new element. NUll if it failed.
  */
-list_t *add_nodeint_end(listint_t **head, const int n)
+list_t *add_nodeint_end(list_t **head, const int n)
 {
 	list_t *new;
 	list_t *temp;
 
 	(void)temp;
 
-	new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
